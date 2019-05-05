fbe29bfd08eb146297995de567b7b3632a69c8ac
Angela Mumba <malaika.mumba@gmail.com>
2019-05-05 21:34:21 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -4,7 +4,15 @@ def countdown(int)
  
   while int > 0 
   puts "#{int} SECOND(S)!"
-  int -=1
+  int -= 1
    end 
   puts  "HAPPY NEW YEAR!"
 end
+
+# def countdown(seconds_to_midnight)
+#   while seconds_to_midnight > 0
+#     puts "#{seconds_to_midnight} SECOND(S)!"
+#     seconds_to_midnight -= 1
+#   end
+#   "HAPPY NEW YEAR!"
+# end
