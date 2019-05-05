a9607c79809ae46cf84b8433703f5f84de152fde
Angela Mumba <malaika.mumba@gmail.com>
2019-05-05 21:43:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -9,13 +9,6 @@ def countdown(int)
   return  "HAPPY NEW YEAR!"
 end
 
-# def countdown(seconds_to_midnight)
-#   while seconds_to_midnight > 0
-#     puts "#{seconds_to_midnight} SECOND(S)!"
-#     seconds_to_midnight -= 1
-#   end
-#   "HAPPY NEW YEAR!"
-# end
 
 def countdown_with_sleep(int)
  
